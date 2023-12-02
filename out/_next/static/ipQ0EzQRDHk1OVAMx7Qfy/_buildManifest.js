f812ca2f3a374f85f0135e3562966b1abc6a1dfa
Gabriel Guerreiro <gabriel@redfox.tech>
2023-12-02 16:53:27 +0000

    Git Ignore

--- out/_next/static/ipQ0EzQRDHk1OVAMx7Qfy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-410bd32c7da46c76.js"],"/404":["static/chunks/pages/404-290aa3b5bf9c1a07.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
